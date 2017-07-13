e0bd31fb9c4956a10bae398dbe0391b5f956bd9a
Daniel Terenzi <danterenzi@hotmail.com>
2017-07-13 01:11:43 -0300

    Commit inicial
    Addon tileset instalado,
    Tilemap e player inicial.

---
+++ characters/mon.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+
+func _integrate_forces(s):
+	pass
+	
+func _ready():
+	pass
---
+++ scripts/player.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+	set_process_input(true)
+	
+func _process(delta):
+	pass
