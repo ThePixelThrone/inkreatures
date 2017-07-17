8a401555079de168d977c1d83c8260f205f8d912
Daniel Terenzi <danterenzi@hotmail.com>
2017-07-17 01:20:13 -0300

    Vídeo da marca, fullscreen

--- scripts/press_start.gd
+++ scripts/press_start.gd
@@ -1,8 +1,20 @@
 extends Control
 
 func _ready():
+	set_process(true)
 	set_process_input(true)
-	
+
+func _process(delta):
+	if (not get_node("VideoPlayer").is_playing()):
+			get_node("VideoPlayer").stop()
+			get_node("VideoPlayer").hide()
+			get_node("background").show()
+
 func _input(event):
-	if (event.is_action("ui_accept")):
-		get_tree().change_scene("res://scenes/MainMenu.tscn")
+	if (event.is_action_pressed("ui_accept")):
+		if (get_node("VideoPlayer").is_playing()):
+			get_node("VideoPlayer").stop()
+			get_node("VideoPlayer").hide()
+			get_node("background").show()
+		else:
+			get_tree().change_scene("res://scenes/MainMenu.tscn")
