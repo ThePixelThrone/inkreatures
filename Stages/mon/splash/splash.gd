4e3648207dab29d01bf5843a67baa3aeb3530b6f
DanMargera <danterenzi@hotmail.com>
2022-10-22 12:16:23 -0300

    Fix ink splash rendering order

--- Stages/mon/splash/splash.gd
+++ Stages/mon/splash/splash.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 var amarelo = Color(1, 0.78, 0, 1)
 var azul = Color(0, 0.45, 1, 1)
@@ -7,18 +7,23 @@ var vermelho = Color(1, 0, 0.19, 1)
 
 func setup(color):
 	if (color == "amarelo"):
-		get_node("Splash").set_color(amarelo)
+		get_node("Node2D/Splash").set_color(amarelo)
 	elif (color == "azul"):
-		get_node("Splash").set_color(azul)
+		get_node("Node2D/Splash").set_color(azul)
 	elif (color == "verde"):
-		get_node("Splash").set_color(verde)
+		get_node("Node2D/Splash").set_color(verde)
 	elif (color == "vermelho"):
-		get_node("Splash").set_color(vermelho)
+		get_node("Node2D/Splash").set_color(vermelho)
+
+func set_position(pos):
+	get_node("Node2D").position = pos
 
 func _ready():
+	# Render on top of other ink splatters
+	layer = get_parent().get_child_count() * -1
 	# Randomize texture
 	var i = randi()%3+1
 	var tex = load("res://assets/images/splash"+var2str(i)+".png")
 	tex.set_flags(0)
-	get_node("Splash").set_texture(tex)
-	get_node("AnimationPlayer").play("Spread")
+	get_node("Node2D/Splash").set_texture(tex)
+	get_node("Node2D/AnimationPlayer").play("Spread")
